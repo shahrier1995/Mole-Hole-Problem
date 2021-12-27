66b32ab888eae405e4b6f645c201e7065fe360f1
shahrier1995 <shahrier.1995@gmail.com>
2021-12-28 00:03:53 +0100

    Some changes with comment

--- Mole_Problem_Solution/Program.cs
+++ Mole_Problem_Solution/Program.cs
@@ -27,9 +27,107 @@ namespace Mole_Test
             floodfill(paramArray, a, b - 1);
             floodfill(paramArray, a + 1, b - 1);
         }
+       
+        public static int Main(string[] args)
+        {
+            var mp = new Program();
+            int[,] mainArray = new int[16, 16];
+            int[,] tempArray = new int[16, 16];
+            
+            
+            for (int i = 0; i < 16; i++)
+            {
+                var drawMap = Console.ReadLine();
+                for (int j = 0; j < 16; j++)
+                {
+                    if (drawMap[j] == '|' || drawMap[j] == '+' || drawMap[j] == '-')
+                    {
+                        mainArray[i, j] = 1;
+                        tempArray[i, j] = 1;
+            
+                    }
+                    else if (drawMap[j] == ' ' || drawMap[j] == '.')
+                    {
+                        mainArray[i, j] = 0;
+                        tempArray[i, j] = 0;
+            
+                    }
+                    else
+                    {
+                        mainArray[i, j] = 2;
+                        tempArray[i, j] = 2;
+            
+                    }
+                }
+            }
+            
+            //Check the mole holes is on fence or two fences are parallel or not
+            
+            //for (int x = 0; x < 16; x++)
+            //{
+            //    for (int y = 0; y < 16; y++)
+            //    {
+            //        if (tempArray[x, y] == 2)
+            //        {
+            //            if (mp.moleOnFence(tempArray, x, y) == false)
+            //            {
+            //                Console.WriteLine("Moles cant be on fence");
+            //                return 0;
+            //            }
+            //        }
+            
+            //        if (tempArray[x, y] == 1)
+            //        {
+            //            if (mp.countFences(tempArray, x, y) == false)
+            //            {
+            //                Console.WriteLine("fences cant be parallel");
+            //                mp.countFences(mainArray, x, y);
+            //                return 0;
+            //            }
+            //        }
+            
+            //    }
+            //}
+            for (int x = 0; x < 16; x++)
+            {
+                for (int y = 0; y < 16; y++)
+                {
+                    if (mainArray[x, y] == 1)
+                    {
+            
+                        if (y == 0)
+                        {
+                            if (mainArray[x, 1] != 1)
+                            {
+                                mp.floodfill(mainArray, x, 1);
+                            }
+            
+                        }
+                        else if (y < 15)
+                        {
+                            if (mainArray[x, y + 1] != 1 && mainArray[x, y - 1] != 1)
+                            {
+                                mp.floodfill(mainArray, x, y + 1);
+                            }
+                        }
+            
+                    }
+                    else if (mainArray[x, y] == 2)
+                    {
+                        mainArray[x, y] = 0;
+                    }
+            
+                }
+            }
+            Console.WriteLine(mp.moleCount.Count);
+            return 0;
+
+        }
+
+        //Functions for checking if a mole hole is on fence or
+        // two parallel fences touch each other. 
         public bool countFences(int[,] paramArray, int a, int b)
         {
-
             int count = 0;
             if (a + 1 > 15 || a - 1 < 0 || b + 1 > 15 || b - 1 < 0)
             {
@@ -99,100 +197,6 @@ namespace Mole_Test
                 }
             }
         }
-        public static int Main(string[] args)
-        {
-            var mp = new Program();
-            int[,] mainArray = new int[16, 16];
-            int[,] tempArray = new int[16, 16];
-
-
-            for (int i = 0; i < 16; i++)
-            {
-                var drawMap = Console.ReadLine();
-                for (int j = 0; j < 16; j++)
-                {
-                    if (drawMap[j] == '|' || drawMap[j] == '+' || drawMap[j] == '-')
-                    {
-                        mainArray[i, j] = 1;
-                        tempArray[i, j] = 1;
-
-                    }
-                    else if (drawMap[j] == ' ' || drawMap[j] == '.')
-                    {
-                        mainArray[i, j] = 0;
-                        tempArray[i, j] = 0;
-
-                    }
-                    else
-                    {
-                        mainArray[i, j] = 2;
-                        tempArray[i, j] = 2;
-
-                    }
-                }
-            }
-
-            //Check the mole holes is on fence or two fences are parallel or not
-
-            //for (int x = 0; x < 16; x++)
-            //{
-            //    for (int y = 0; y < 16; y++)
-            //    {
-            //        if (tempArray[x, y] == 2)
-            //        {
-            //            if (mp.moleOnFence(tempArray, x, y) == false)
-            //            {
-            //                Console.WriteLine("Moles cant be on fence");
-            //                return 0;
-            //            }
-            //        }
-
-            //        if (tempArray[x, y] == 1)
-            //        {
-            //            if (mp.countFences(tempArray, x, y) == false)
-            //            {
-            //                Console.WriteLine("fences cant be parallel");
-            //                mp.countFences(mainArray, x, y);
-            //                return 0;
-            //            }
-            //        }
-
-            //    }
-            //}
-            for (int x = 0; x < 16; x++)
-            {
-                for (int y = 0; y < 16; y++)
-                {
-                    if (mainArray[x, y] == 1)
-                    {
-
-                        if (y == 0)
-                        {
-                            if (mainArray[x, 1] != 1)
-                            {
-                                mp.floodfill(mainArray, x, 1);
-                            }
-
-                        }
-                        else if (y < 15)
-                        {
-                            if (mainArray[x, y + 1] != 1 && mainArray[x, y - 1] != 1)
-                            {
-                                mp.floodfill(mainArray, x, y + 1);
-                            }
-                        }
-
-                    }
-                    else if (mainArray[x, y] == 2)
-                    {
-                        mainArray[x, y] = 0;
-                    }
-
-                }
-            }
-            Console.WriteLine(mp.moleCount.Count);
-            return 0;
-
-        }
     }
+
 }
